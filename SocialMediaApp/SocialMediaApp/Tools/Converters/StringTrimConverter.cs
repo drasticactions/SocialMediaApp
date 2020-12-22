01ec051cb44d13f0d48d0727e2e91025a9c3d730
Tim Miller <innerlogic4321@gmail.com>
2020-12-22 08:37:36 -0500

    Edit Theme

---
+++ SocialMediaApp/SocialMediaApp/Tools/Converters/StringTrimConverter.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="StringTrimConverter.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SocialMediaApp.Tools.Converters
+{
+    /// <summary>
+    /// String Trim Converter.
+    /// </summary>
+    public class StringTrimConverter : IValueConverter
+    {
+        /// <inheritdoc/>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string stringValue)
+            {
+                return stringValue.Trim();
+            }
+
+            return value;
+        }
+
+        /// <inheritdoc/>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
